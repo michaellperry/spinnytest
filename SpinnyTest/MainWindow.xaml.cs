e19c94b156b2b1e6ef24e47763016fdf22d1efd6
Michael L Perry <michael@qedcode.com>
2013-06-12 13:55:48 -0500

    Play animation while the control is visible.

---
+++ SpinnyTest/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SpinnyTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Spin_Click(object sender, RoutedEventArgs e)
+        {
+            spinnyBar.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            spinnyBar.Visibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
